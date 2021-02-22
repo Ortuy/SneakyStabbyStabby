f2742a8c7e5a27c56cba75af046929899f13879d
Ortuy <Ortuy@users.noreply.github.com>
2021-02-22 21:46:54 +0100

    Main menu i guess

---
+++ Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private string versionName = "2021.02.22";
+
+    [SerializeField] private GameObject connectPanel;
+    [SerializeField] private InputField createGameInput;
+    [SerializeField] private InputField joinGameInput;
+
+    //[SerializeField] private GameObject startButton;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(versionName);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("I'm Connected!");
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(createGameInput.text, new RoomOptions() { maxPlayers = 2 }, null);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom(joinGameInput.text, roomOptions, TypedLobby.Default);
+    }
+
+    private void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Arena");
+    }
+}
