115f8f466be6319ba154a45fac1eec79348f32bf
Ortuy <Ortuy@users.noreply.github.com>
2021-03-10 00:58:21 +0100

    Some art experiments

---
+++ Project/Assets/Scripts/Vegetation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Vegetation : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private ParticleSystem particles;
+
+    private bool animationPlaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!animationPlaying)
+        {
+            animator.SetTrigger("Rustle");
+            particles.Play();
+            animationPlaying = true;
+        }
+        
+    }
+
+    public void MarkAnimationEnd()
+    {
+        animationPlaying = false;
+    }
+}
