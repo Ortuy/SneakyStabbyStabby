f31232a209c97645bf400c197bf98cf1fd54b526
Damien1998 <damianbombala98@gmail.com>
2021-03-13 20:42:15 +0100

    bomb

--- Project/Assets/Scripts/Gas.cs
+++ Project/Assets/Scripts/Gas.cs
@@ -6,24 +6,104 @@ using Photon.Pun;
 public class Gas : MonoBehaviourPunCallbacks
 {
     public Rigidbody2D rb;
-    public GameObject gas;
+    //public GameObject gas;
     private Vector3 scaleChange;
+    public bool timerInhaleRunning = false;
+    public float timeInhelRemaining = 5;
+    public float toxinDamage = 1;
+    public bool ded = false;
+    
+
+    [PunRPC]
+    public void DestroyObject()
+    {
+        Destroy(this.gameObject);
+    }
 
     private void Awake()
     {
-        scaleChange = new Vector3(-0.01f, -0.01f,0f);
+        scaleChange = new Vector3(0.01f, 0.01f,0f);
+        StartCoroutine("DestroyByTime");
+
     }
     void Update()
     {
-        gas.transform.localScale += scaleChange;
+        /*gas.*/transform.localScale += scaleChange;
         
 
-        // Move upwards when the sphere hits the floor or downwards
-        // when the sphere scale extends 1.0f.
-        if (gas.transform.localScale.y < 0.1f || gas.transform.localScale.y > 1.0f)
+        
+        if (/*gas.*/transform.localScale.y > 1.0f)
         {
-            scaleChange = -scaleChange;
+            scaleChange = new Vector3(0f, 0f, 0f);
+            
+
+        }
+        
+        if (timerInhaleRunning == true)
+        {
+            if (timeInhelRemaining > 0)
+            {
+                timeInhelRemaining -= Time.deltaTime;
+                ded = false;
+            }
+            else
+            {
+
+                timeInhelRemaining = 5;
+                timerInhaleRunning = false;
+                ded = true;
+                
+            }
+        }
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PhotonView target = collision.gameObject.GetComponent<PhotonView>();
+        if (target != null)
+        {
+            if (target.tag == "Player")
+            {
+                timerInhaleRunning = true;
+                if(ded == true)
+                {
+                    target.RPC("ReduceHealth", RpcTarget.AllBuffered, toxinDamage);
+                }
+
+                
+            }
+
             
         }
+        
+        
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PhotonView target = collision.gameObject.GetComponent<PhotonView>();
+        if (target != null)
+        {
+            if (target.tag == "Player")
+            {
+                timerInhaleRunning = false;
+                ded = false;
+                timeInhelRemaining = 5;
+
+
+
+            }
+
+
+        }
+
+
+    }
+    IEnumerator DestroyByTime()
+    {
+        yield return new WaitForSeconds(10f);
+        photonView.RPC("DestroyObject", RpcTarget.AllBuffered);
+
+    }
+
 }
