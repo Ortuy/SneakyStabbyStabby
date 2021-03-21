c9979d27af10ab186cf477748493587680d558b2
Damien1998 <damianbombala98@gmail.com>
2021-03-21 17:23:03 +0100

    Geltrap i zalążki blinka

---
+++ Project/Assets/Scripts/Paint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Paint : MonoBehaviourPunCallbacks
+{
+    public Rigidbody2D rb;
+    public float paintDestroyTime;
+
+    [PunRPC]
+    public void DestroyObject()
+    {
+        Destroy(this.gameObject);
+    }
+    private void Awake()
+    {
+        
+        StartCoroutine("DestroyByTime");
+
+    }
+    IEnumerator DestroyByTime()
+    {
+        yield return new WaitForSeconds(paintDestroyTime);
+        photonView.RPC("DestroyObject", RpcTarget.AllBuffered);
+
+    }
+
+}
