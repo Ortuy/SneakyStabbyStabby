165816dc8a8243ddc415659f20aa1e4d785da24d
Ortuy <Ortuy@users.noreply.github.com>
2021-04-03 12:56:08 +0200

    Footstep bugfix

---
+++ Project/Assets/Scripts/Footstep.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Footstep : MonoBehaviourPunCallbacks
+{
+    [PunRPC]
+    public void SetFootstep(bool flip, float duration)
+    {
+        GetComponent<SpriteRenderer>().flipX = flip;
+        Destroy(gameObject, duration);
+    }
+}
