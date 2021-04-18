90778b565c38450690fe84626feb2f211eeca21e
Damien1998 <damianbombala98@gmail.com>
2021-04-18 11:46:44 +0200

    Revert "Revert "pierdoly""
    This reverts commit c3fb82e6a6d478d336118ad173aa06fb06afe38d.

---
+++ Project/Assets/Scripts/Gold.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Gold : MonoBehaviourPunCallbacks
+{
+    public Player player;
+
+    public void OnTriggerEnter2D(Collider2D Player)
+    {
+
+        if (Player.tag == "Player")
+        {
+            player.gold += 5; 
+            Debug.Log("yee");
+        }
+    }
+}
