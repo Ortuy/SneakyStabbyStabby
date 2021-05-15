4de18d0df88ca860069ac0aacdfe563206dd14d5
Ortuy <Ortuy@users.noreply.github.com>
2021-05-15 11:50:59 +0200

    Vegetation sound fix

--- Project/Assets/Scripts/Vegetation.cs
+++ Project/Assets/Scripts/Vegetation.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum VegetationType { NONE, PLANT_BIG, PLANT_SMALL, SHROOMS, PUDDLE, BIRDS }
+
 [RequireComponent(typeof(Animator))]
 public class Vegetation : MonoBehaviour
 {
     private Animator animator;
     [SerializeField] private ParticleSystem particles;
     [SerializeField] private bool moveParticlesToCollision;
+    [SerializeField] private VegetationType vegetationType;
 
     private bool animationPlaying;
 
@@ -40,9 +43,27 @@ public class Vegetation : MonoBehaviour
 
     public void PlayVegetationSound(ParticleSystem particles)
     {
-        if (particles.ToString().Contains("Spores"))
-            AkSoundEngine.PostEvent("amb_schroom_enter", gameObject, gameObject);
-        else if (particles.ToString().Contains("PickupParticle"))
-            AkSoundEngine.PostEvent("amb_bush_rustle_enter", gameObject, gameObject);
+        //if (particles.ToString().Contains("Spores"))
+        //    AkSoundEngine.PostEvent("amb_schroom_enter", gameObject, gameObject);
+        //else if (particles.ToString().Contains("PickupParticle"))
+        //    AkSoundEngine.PostEvent("amb_bush_rustle_enter", gameObject, gameObject);
+        switch(vegetationType)
+        {
+            case VegetationType.PLANT_BIG:
+                AkSoundEngine.PostEvent("amb_bush_rustle_enter", gameObject, gameObject);
+                break;
+            case VegetationType.PLANT_SMALL:
+                //Put code to play the small plant rustle sound here!
+                break;
+            case VegetationType.SHROOMS:
+                AkSoundEngine.PostEvent("amb_schroom_enter", gameObject, gameObject);
+                break;
+            case VegetationType.PUDDLE:
+                //Put code to play the puddle splash sound here!
+                break;
+            case VegetationType.BIRDS:
+                //Put code to play the flying birds sound here!
+                break;
+        }
     }
 }
